bbe46137ea9762da4867cf8dad2e8710dbe1a9e1
luizfiuzaa <luizffreitas2006@gmail.com>
2025-07-17 21:03:17 -0300

    :rocket: learning 'if' and 'else'

--- module-2/main.go
+++ module-2/main.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main(){
-	for i := 0; i <= 10; i++ {
-		fmt.Printf("%v \n",i)
+	
+	for i := 1; i <= 10; i++ {
+		var result string
+		if(i % 2 == 0){
+			result = "even" 
+		}else{
+			result = "odd" 
+		}
+		fmt.Printf("%v (%s)\n", i, result)
 	}
 }
+
