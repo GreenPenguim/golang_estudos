c23f073fd9730ce916cc96e0af5e50b934a9ebd8
luizfiuzaa <luizffreitas2006@gmail.com>
2025-07-18 13:07:45 -0300

    :rocket: learning loops, loop hierarchy and how to make a 'while'

--- module-2/main.go
+++ module-2/main.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main(){
-	
+	loop1()
+	fmt.Println("-----")
+	loop2()
+	fmt.Println("-----")
+	loop3()
+}
+
+//for with validations if and switchcase
+func loop1(){
 	for i := 1; i <= 10; i++ {
 		var result string
 		if(i % 2 == 0){
@@ -23,3 +31,22 @@ func main(){
 	}
 }
 
+
+// Loop hierarchy
+func loop2(){
+	for hours:=0; hours<=12; hours++{
+		fmt.Printf("Hour: %d ", hours)
+		for minutes:=0;minutes<60; minutes++{
+			fmt.Printf("Minute: %d\n", minutes)
+		}
+	}
+}
+
+// In golang, we dont have a 'while', otherwise we can use 'for' loop as 'while' 
+func loop3(){
+	i:=0
+	for i<=5 {
+		fmt.Println("Looping:", i)
+		i++
+	}
+}
