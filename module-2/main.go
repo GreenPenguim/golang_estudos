705bd205535051d1daedd7c067d576688a28b774
luizfiuzaa <luizffreitas2006@gmail.com>
2025-07-22 10:50:31 -0300

    :rocket: learning about 'structs, methods and interfaces'

--- module-2/main.go
+++ module-2/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main(){
 	/*
-		See definintions on: .\module-1\main.go line 14
+		See definintions on: .\module-1\main.go line 39
 	*/
 	fmt.Println("Loop1")
 	loop1()
@@ -23,6 +26,15 @@ func main(){
 	fmt.Println("-----")
 	fmt.Println("Maps")
 	maps()
+	fmt.Println("-----")
+	fmt.Println("Struct")
+	structs()
+	fmt.Println("-----")
+	fmt.Println("Method")
+	methods()
+	fmt.Println("-----")
+	fmt.Println("Interface")
+	interfaces()
 }
 
 //for with validations if and switchcase
@@ -120,5 +132,72 @@ func maps(){
 	elements["Li"] = "Lithium"
 
 	fmt.Println(elements["Li"])
+}
 
+func structs(){
+	type Person struct{
+		name string
+		age int
+	}
+
+	fmt.Println(Person{"John", 30})
+	fmt.Println(Person{"Alice", 25})
+}
+
+// Methods are functions that are associated with a type
+type Rectangle struct {
+	width, height float64
+}
+func (r *Rectangle) area() float64 {
+	return r.width * r.height
+}
+func (r Rectangle) perimeter() float64 {
+	return 2 * (r.width + r.height)
+}
+
+func methods(){
+	r:= Rectangle{width: 50, height: 25}
+	fmt.Printf("Area: %.2f\n", r.area())
+	fmt.Printf("Perimeter: %.2f\n", r.perimeter())
+}
+
+type Shape interface{
+	area() float64
+	perimeter() float64
+}
+
+type Square struct{
+	side float64
+}
+func (s Square) area() float64 {
+	return s.side * s.side
+}
+func (s Square) perimeter() float64 {
+	return 4 * s.side
+}
+
+type Circle struct {
+	radius float64
+}
+func (c Circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+func (c Circle) perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+
+func measure(s Shape){
+	fmt.Println("Shape:", s)
+	fmt.Println("Area:", s.area())
+	fmt.Println("Perimeter:", s.perimeter())
+}
+
+func interfaces(){
+	s := Square{10}
+	c := Circle{5}
+
+	measure(s)
+	println("::::")
+	measure(c)
 }
