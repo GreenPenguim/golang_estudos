07a6d0fe8755b735fd6e7b94b81fbaf99990dcff
luizfiuzaa <luizffreitas2006@gmail.com>
2025-08-01 17:30:55 -0300

    :rocket: resolving ping-pong exercise

--- exercices/mod-3/ex3.go
+++ exercices/mod-3/ex3.go
@@ -2,6 +2,41 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main(){}
+/*
+	Ping-Pong:
+	This example demonstrates how to use goroutines and channels to create a simple ping-pong game.
+*/
+
+func main(){
+	var c = make(chan string)
+	go ping(c)
+	go print(c)
+	go pong(c)
+
+	var exit string
+	fmt.Println("Press Enter to exit...")
+	fmt.Scanln(&exit)
+}
+
+func ping(c chan string){
+	for i:= 0; ; i++{
+		c <- "ping"
+	}
+}
+
+func pong(c chan string){
+	for i:= 0; ; i++{
+		c <- "pong"
+	}
+}
+
+func print(c chan string){
+	for {
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
+	}
+}
